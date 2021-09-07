5322a318193793b94743729b96cb727792f6662e
HamsterOh <mariusnouchet@gmail.com>
2021-09-08 00:37:33 +0200

    Code documentation Fixed threads count Display execution time on threads end Only scan for 25565 due of a performance issue

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import threading
 import time
-from dns.rdatatype import NULL
 import nmap
 from mcstatus import MinecraftServer
+
+# Determines the IP address from the given integer using bitwise operations
+def intToIP(num):
+    return (str((num >> 24) & 0xFF) + "."
+            + str((num >> 16) & 0xFF) + "."
+            + str((num >> 8) & 0xFF) + "."
+            + str(num & 0xFF))
 
 blacklistedPools = [
     [167772160, 184549375], # 10.0.0.0 – 10.255.255.255
@@ -10,12 +16,6 @@ blacklistedPools = [
     [3232235520, 3232301055], # 192.168.0.0 – 192.168.255.255
     [2130706433, 2130706433] # 127.0.0.1
 ]
-
-def intToIP(num):
-    return (str((num >> 24) & 0xFF) + "."
-            + str((num >> 16) & 0xFF) + "."
-            + str((num >> 8) & 0xFF) + "."
-            + str(num & 0xFF))
 
 def intIsBlacklisted(num):
     for pool in blacklistedPools:
@@ -25,32 +25,31 @@ def intIsBlacklisted(num):
     return None
 
 def thread(num):
+    startTime = time.time()
     print("Start thread", threading.current_thread().name)
     nm = nmap.PortScanner()
-    max = 4294967295
-    curr = round(max/(num+1))
+    max = 4294967295 # Maximal IP address in integer to reach
+    curr = round(max/(num+1)) # Current IP address in integer depeding on thread number
     while curr < round(max/num):
-        curr += 1
+        curr += 1 # Not at the end of the loop beacuse of the next condition passing the while loop
         blacklistedPool = intIsBlacklisted(curr)
         if blacklistedPool is not None: # Is ip in blacklisted pool?
             curr += blacklistedPool[1] - blacklistedPool[0] - 1 # Minus one because we are going to increase at the begin of the loop
             continue
         ip = intToIP(curr)
-        print("Scanning", ip)
         try:
-            for port in nm.scan(ip, "1-7777")["scan"][ip]["tcp"].keys():
+            for port in nm.scan(ip, "25565")["scan"][ip]["tcp"].keys():
                 try: # Is a minecraft server running on that port?
                     server = MinecraftServer.lookup(ip + ":" + port)
                     print("Server found:", server.host, ":", port)
                 except:
                     pass
         except:
+            print("nope")
             pass
-        time.sleep(2)
-    print("End thread", threading.current_thread().name)
+        time.sleep(2) # Wait 2 seconds before scanning the next IP
+    print("End thread", threading.current_thread().name, (time.time() - startTime)) # Display execution time
 
 if __name__ == '__main__':
-    startTime = time.time()
-    for i in range(1, 10):
+    for i in range(0, 10): # Start X threads
         threading.Thread(name=i, target=thread, args=[i]).start()
-    print("Executed in", (time.time() - startTime), "seconds")
